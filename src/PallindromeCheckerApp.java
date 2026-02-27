b0dec9b2e3a86634d6b0d5be45d9eb7fbd96eb11
aa0452-art <aa0452@srmist.edu.in>
2026-02-27 16:22:36 +0530

    UC1: Add welcome message module

---
+++ src/PallindromeCheckerApp.java
@@ -0,0 +1,13 @@
+public class PallindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println("        PALINDROME CHECKER APP        ");
+        System.out.println("======================================");
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Application Version : 1.0.0");
+        System.out.println("Status : Application Started Successfully");
+        System.out.println("======================================");
+    }
+}
