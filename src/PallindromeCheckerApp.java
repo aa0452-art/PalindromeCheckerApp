46d74f654cd417ea3c982cdf0dbc225b3a40b468
aa0452-art <aa0452@srmist.edu.in>
2026-02-27 16:34:48 +0530

    UC1: Add welcome message module

--- src/PallindromeCheckerApp.java
+++ src/PallindromeCheckerApp.java
@@ -1,13 +1,22 @@
-public class PallindromeCheckerApp {
+public class pallindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("======================================");
-        System.out.println("        PALINDROME CHECKER APP        ");
-        System.out.println("======================================");
-        System.out.println("Application Name : Palindrome Checker");
-        System.out.println("Application Version : 1.0.0");
-        System.out.println("Status : Application Started Successfully");
-        System.out.println("======================================");
+        // Hardcoded string (String Literal)
+        String word = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check palindrome using if-else
+        if (word.equals(reversed)) {
+            System.out.println("The word \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is NOT a Palindrome.");
+        }
     }
 }
